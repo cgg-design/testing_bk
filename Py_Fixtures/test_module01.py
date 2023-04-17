e96859afabf69f9fd6c53ce21fca479ff3ebc2d4
cgg-design <c.guetsop@gmail.com>
2023-04-17 20:28:00 +0200

    Worst case, 1th fail -x

--- Py_Fixtures/test_module01.py
+++ Py_Fixtures/test_module01.py
@@ -1,5 +1,5 @@
 def test_a1():
-    assert 4 < 5
+    assert 5 < 5
 
 def test_a2():
     assert True
@@ -8,7 +8,7 @@ def test_a3():
     assert "abcsdd" == "abcsdd"
 
 def test_a4():
-    assert [1,2,3] == [1,2,3]
+    assert [1,2,3] == [1,2,4]
 
 def test_a5():
     assert [1,2] < [1,2,3]
