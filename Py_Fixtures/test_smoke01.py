afe8a90bc77df2160865196fb9e840a5f7b4c6fb
cgg-design <c.guetsop@gmail.com>
2023-04-20 22:30:13 +0200

    change

--- Py_Fixtures/test_smoke01.py
+++ Py_Fixtures/test_smoke01.py
@@ -5,5 +5,5 @@ pytestmark = pytest.mark.smoke
 def test_case01(setup_smoke):
     print('\n')
     print(setup_smoke[1:3])
-    assert setup_smoke[0] == 'New York1'
+    assert setup_smoke[0] == 'New York'
     assert setup_smoke[::2] == ['New York','Berlin','Singapor','Pekin']
